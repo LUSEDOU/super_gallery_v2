91fbda44004f6fb98d58a2b2b7f202f082fa5358
LUSEDOU <dolorier_uribe@hotmail.com>
2022-08-10 19:56:23 -0500

    fix minor infos

--- lib/gallery/cubit/gallery_cubit.dart
+++ lib/gallery/cubit/gallery_cubit.dart
@@ -1,5 +1,3 @@
-import 'dart:developer';
-
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:images_repository/images_repository.dart';
@@ -12,14 +10,12 @@ class GalleryCubit extends Cubit<GalleryState> {
   final ImagesRepository imagesRepository;
 
   Future<void> init() async {
-    print(state.images);
     emit(
       state.copyWith(
         images: imagesRepository.getImages(),
         status: GalleryStatus.loading,
       ),
     );
-    print(state.images);
   }
 
   void imagesCached() {
